92f41750e3a4e6de761e6d29faf45d3c7cb144c9
FPSensor <gkartyt@gmail.com>
2023-11-30 00:00:11 +0000

    devon: Ancient-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_devon.mk
+    $(LOCAL_DIR)/ancient_devon.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_devon-user \
-    lineage_devon-userdebug \
-    lineage_devon-eng
+    ancient_devon-user \
+    ancient_devon-userdebug \
+    ancient_devon-eng
